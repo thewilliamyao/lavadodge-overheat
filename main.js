d10591ac1fc1837571eba618be814f92a8b82573
wyao7 <wyao7@jhu.edu>
2017-01-23 03:59:13 -0500

    trying to hide mobile address bar

--- main.js
+++ main.js
@@ -417,3 +417,20 @@ var GameState = {
 
 game.state.add('GameState', GameState);
 game.state.start('GameState');
+
+
+function hideAddressBar()
+{
+    if(!window.location.hash)
+    {
+        if(document.height < window.outerHeight)
+        {
+            document.body.style.height = (window.outerHeight + 50) + 'px';
+        }
+
+        setTimeout( function(){ window.scrollTo(0, 1); }, 50 );
+    }
+}
+
+window.addEventListener("load", function(){ if(!window.pageYOffset){ hideAddressBar(); } } );
+window.addEventListener("orientationchange", hideAddressBar );
